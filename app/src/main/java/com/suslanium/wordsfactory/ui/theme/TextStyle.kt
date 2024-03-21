1929f3c890159242abbfe755b68da8d05a5cf34e
Suslanium <zaharov22m@gmail.com>
2024-03-21 13:43:41 +0700

    Pictures, colors and text styles

---
+++ app/src/main/java/com/suslanium/wordsfactory/ui/theme/TextStyle.kt
@@ -0,0 +1,82 @@
+package com.suslanium.wordsfactory.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val HeadingH1 = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W700,
+    fontSize = 56.sp,
+    lineHeight = 59.sp
+)
+
+val HeadingH2 = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W700,
+    fontSize = 40.sp,
+    lineHeight = 46.sp
+)
+
+val HeadingH3 = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W700,
+    fontSize = 32.sp,
+    lineHeight = 42.sp
+)
+
+val HeadingH4 = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W500,
+    fontSize = 24.sp,
+    lineHeight = 32.sp
+)
+
+val HeadingH5 = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W500,
+    fontSize = 20.sp,
+    lineHeight = 26.sp
+)
+
+val ParagraphLarge = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W400,
+    fontSize = 16.sp,
+    lineHeight = 26.sp
+)
+
+val ParagraphMedium = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W400,
+    fontSize = 14.sp,
+    lineHeight = 21.sp
+)
+
+val ParagraphSmall = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W400,
+    fontSize = 12.sp,
+    lineHeight = 18.sp
+)
+
+val ButtonLarge = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W500,
+    fontSize = 18.sp,
+    lineHeight = 22.sp
+)
+
+val ButtonMedium = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W500,
+    fontSize = 16.sp,
+    lineHeight = 18.sp
+)
+
+val ButtonSmall = TextStyle(
+    fontFamily = Rubik,
+    fontWeight = FontWeight.W500,
+    fontSize = 14.sp,
+    lineHeight = 16.sp
+)
